220c1b02e560d14e7359ea3b624a7c00ec992ad4
LR-DEFENDER <as9887831202@gmail.com>
2023-11-12 06:42:22 +0530

    newLoadingIconsAndOptimisations

--- script.js
+++ script.js
@@ -5,17 +5,24 @@ const comicStrip = document.getElementById("comic-strip");
 generateButton.addEventListener("click", () => {
     userInputArray.length = 0; // Clear the array
     for (let i = 1; i <= 10; i++) {
-        const comicPanel = document.getElementById(`img${i}`);
-        comicPanel.setAttribute("width","25%");
-        comicPanel.setAttribute("height", "25%");
-        comicPanel.src = "./Hourglass.gif";
         const inputField = document.getElementById(`input-${i}`);
         const userInput = inputField.value;
         console.log(userInput);
-        userInputArray.push(userInput);
+        if (userInput != "") {
+            userInputArray.push(userInput);
+        }
+       
     }
-    // generateComicStripWithLoadingIcons();
-    generateComicStrip();
+    if (userInputArray.length == 10) {
+         for (let i = 1; i <= 10; i++) {
+           const comicPanel = document.getElementById(`img${i}`);
+           comicPanel.setAttribute("width", "100%");
+           comicPanel.setAttribute("height", "100%");
+           comicPanel.src = "./loading_colorful.gif";
+          
+         }
+       generateComicStrip();
+     }
 });
 async function query(data) {
 	const response = await fetch(
@@ -35,75 +42,20 @@ async function query(data) {
 }
 async function generateComicStrip() {
     // comicStrip.innerHTML = ""; // Clear previous comic strip
-    let i = 1;
-    for (const userInput of userInputArray) {
-        console.log(`Loading ${i}`);
-        const comicPanel = document.getElementById(`img${i}`);
-        comicPanel.src="./ZZ5H.gif"
-        const image = await query({ "inputs": userInput }); // Call the query function
-        
-        // Create an image element for each panel of the comic
-        // const comicPanel = document.createElement("img");
-        comicPanel.setAttribute("width","100%");
-        comicPanel.setAttribute("height", "100%");
-        comicPanel.src = URL.createObjectURL(image);
-        comicPanel.alt = userInput; // Set alt text to the user's input
-        
-        // Append the image to the comic strip
-        //comicStrip.appendChild(comicPanel);
-        i++;
-    }
-}
+    if (userInputArray.length < 10) { }
+    else {
+        let i = 1;
+        for (const userInput of userInputArray) {
+            console.log(`Loading ${i}`);
+            const comicPanel = document.getElementById(`img${i}`);
+            comicPanel.src = "./loading_circle_strip.gif";
+            const image = await query({ "inputs": userInput }); 
 
-// function generateComicStripWithLoadingIcons() {
-//     comicStrip.innerHTML = ""; // Clear previous comic strip
-
-//     for (let i = 0; i < 10; i++) {
-//         // Create an image element for each panel with a loading icon
-//         const comicPanel = document.createElement("img");
-//         comicPanel.src = 'loading-icon.gif'; // Replace 'loading-icon.gif' with your loading icon URL
-//         comicPanel.alt = 'Loading...';
-
-//         // Append the image to the comic strip
-//         comicStrip.appendChild(comicPanel);
-       
-
-//         // Generate image for the panel
-//         generateImageForPanel(comicPanel, userInputArray[i], i);
-//     }
-// }
-// function generateComicStripWithLoadingIcons() {
-//   comicStrip.innerHTML = ""; // Clear previous comic strip
-
-//   for (let i = 0; i < 10; i++) {
-//     const inputField = document.getElementById(`input-${i + 1}`);
-//     const userInput = inputField.value.trim(); // Get input text and remove leading/trailing spaces
-
-//     if (userInput === "") {
-//       // Handle empty input and highlight the empty input container
-//       inputField.classList.add("empty-input");
-//       return; // Stop generating the comic strip if any input is empty
-//     } else {
-//       // Create an empty image container with a loading icon
-//       inputField.classList.remove("empty-input"); // Remove highlight
-//       const comicPanel = document.createElement("img");
-//       comicPanel.src = "loading-icon.gif"; // Replace 'loading-icon.gif' with your loading icon URL
-//       comicPanel.alt = "Loading...";
-
-//       // Append the image container to the comic strip
-//       comicStrip.appendChild(comicPanel);
-//     }
-//   }
-//}
-
-// Function to generate image for a panel
-async function generateImageForPanel(comicPanel, userInput, panelIndex) {
-    const image = await query({ "inputs": userInput });
-
-    // Replace the loading icon with the actual image
-    comicPanel.src = URL.createObjectURL(image);
-    comicPanel.alt = userInput;
-    if (panelIndex === 9) {
-        // Do something when the comic strip is fully loaded
+            comicPanel.setAttribute("width", "100%");
+            comicPanel.setAttribute("height", "100%");
+            comicPanel.src = URL.createObjectURL(image);
+            comicPanel.alt = userInput;
+            i++;
+        }
     }
 }
